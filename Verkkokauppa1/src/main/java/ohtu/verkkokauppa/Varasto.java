32527f2562a4ceba70e27503f9e9fadceebf38ea
doc97 <daniel.riissanen@gmail.com>
2018-11-06 08:57:29 +0200

    Refactor Varasto to use annotations

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,12 +1,17 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 
+@Component
 public class Varasto implements VarastoInterface {
 
     private KirjanpitoInterface kirjanpito;
     private HashMap<Tuote, Integer> saldot;  
-    
+
+    @Autowired
     public Varasto(KirjanpitoInterface kirjanpito) {
         this.kirjanpito = kirjanpito;
         saldot = new HashMap<>();
